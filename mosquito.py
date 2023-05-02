55e2c42a1534c9fbd06d1ef47fcc574b70445e68
Sarah <sarah.barbosaa@gmail.com>
2023-05-02 12:29:21 -0300

    subindo projeto teste

---
+++ mosquito.py
@@ -0,0 +1,12 @@
+#o mosquito é da dengue?
+
+mosquito = input ("o mosquito tem as patinhas listradas? ")
+listras = input ("as listras são pretas e brancas? " )
+
+# e se eu quiser colocar uma resposta pro caso de o mosquito não ter as patinhas listradas?
+
+
+if (mosquito == "sim" and listras == "sim") :
+    print ("cuidado, esse mosquito é da dengue!")
+else :
+    print ("não se preocupe, é só um pernilongo comum!")
